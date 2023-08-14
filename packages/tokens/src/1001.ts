85ad433b805457d13b0c66e064c0b18d729aaef5
LCCHERRY <125979770+LCCHERRY@users.noreply.github.com>
2023-08-14 16:32:11 +0900

    Update 1001.ts

--- packages/tokens/src/1001.ts
+++ packages/tokens/src/1001.ts
@@ -6,5 +6,6 @@ export const baobabTokens = {
   odi: new Token(ChainId.BAOBAB, '0xdE2e61AccDe6e7C9a0061D2B80427B6F2710FFEf', 18, 'ODI', 'ODI Token', ''),
   usdt: new Token(ChainId.BAOBAB, '0x80Fc0d8652C784f1C5289FBea90f77Ba474B489f', 18, 'oUSDT', 'oUSDT Token', ''),
   lcg: new Token(ChainId.BAOBAB, '0x5afF4A709530Bd5b21915e5D14a73e8e0F613D1C', 18, 'LCG', 'LCG Token', ''),
+  aaa: new Token(ChainId.BAOBAB, '0x9967a82d580a94724471ce331c8eb7b882e41ac5', 18, 'AAA', 'AAA Token', ''),
   usdc: USDC_BAOBAB
 }
