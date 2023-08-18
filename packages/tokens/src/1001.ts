6a14476ce85786879cbc71f9f74cdca079a3be69
LCCHERRY <142189290+ODiSWAP@users.noreply.github.com>
2023-08-18 16:20:06 +0900

    Update 1001.ts

--- packages/tokens/src/1001.ts
+++ packages/tokens/src/1001.ts
@@ -5,7 +5,7 @@ export const baobabTokens = {
   weth: WETH9[ChainId.BAOBAB],
   odi: new Token(ChainId.BAOBAB, '0xdE2e61AccDe6e7C9a0061D2B80427B6F2710FFEf', 18, 'ODI', 'ODI Token', ''),
   usdt: new Token(ChainId.BAOBAB, '0x80Fc0d8652C784f1C5289FBea90f77Ba474B489f', 18, 'oUSDT', 'oUSDT Token', ''),
-  lcg: new Token(ChainId.BAOBAB, '0x5afF4A709530Bd5b21915e5D14a73e8e0F613D1C', 18, 'LCG', 'LCG Token', ''),
+  lcg: new Token(ChainId.BAOBAB, '0xccd181cab7bd20fb7b4b4fdf6d8fe0f77dd6db35', 18, 'LCG', 'LCG Token', ''),
   aaa: new Token(ChainId.BAOBAB, '0x9967a82d580a94724471ce331c8eb7b882e41ac5', 18, 'AAA', 'AAA Token', ''),
   usdc: USDC_BAOBAB
 }
