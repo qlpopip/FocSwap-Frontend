bb7b6e62499639304c32ae517d8954d776bd0e94
ODiSWAP <142189290+ODiSWAP@users.noreply.github.com>
2023-08-25 15:33:16 +0900

    oETH 추가 및 팜생성

--- packages/tokens/src/8217.ts
+++ packages/tokens/src/8217.ts
@@ -6,4 +6,5 @@ export const klaytnTokens = {
     odi: new Token(ChainId.KLAYTN, '0x044925B7c6AA8EF03Ad294Bc48EDC1a2c3877a6D', 18, 'ODI', 'ODI Token', ''),
     usdt: USDT_KLAYTN,
     lcg: new Token(ChainId.KLAYTN, '0xEf632E070Df56371Ad33A85148De398104EC55aB', 18, 'LCG', 'LCG Token', ''),
+    oETH: new Token(ChainId.KLAYTN, '0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167', 18, 'oETH', 'ETH', ''),
 }
