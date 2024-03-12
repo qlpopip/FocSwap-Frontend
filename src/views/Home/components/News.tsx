342b360c60ce204b85041072a25b4e3eace30411
meritz3695 <meritz3695@gmail.com>
2024-03-13 05:53:35 +0900

    fix: News Component: use react-items-carousel

--- src/views/Home/components/News.tsx
+++ src/views/Home/components/News.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { Flex, Heading } from '@pancakeswap/uikit'
-import { useWeb3React } from '@pancakeswap/wagmi'
 import { useTranslation } from '@pancakeswap/localization'
-import useTheme from 'hooks/useTheme'
 import styled, { css } from 'styled-components'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import ItemsCarousel from 'react-items-carousel'
 
 const newsFeedData = [
   {
@@ -44,15 +43,6 @@ const newsFeedData = [
       '4: Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     link: '/',
   },
-  {
-    id: 5,
-    image: '/images/news_example.png',
-    author: 'Tim Johnson',
-    date: '2023-03-07',
-    title:
-      '5: Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: '/',
-  },
 ]
 
 const formatDate = (dateString: string): string => {
@@ -60,21 +50,6 @@ const formatDate = (dateString: string): string => {
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
   return date.toLocaleDateString('en-US', options)
 }
-
-const MobileFlex = styled.div`
-  position: relative;
-  display: flex;
-  gap: 2rem;
-  margin: 4rem 0;
-  justify-content: center;
-
-  ${css`
-    @media screen and (max-width: 768px) {
-      flex-direction: column-reverse;
-      align-items: center;
-    }
-  `}
-`
 
 const StyledFlex = styled(Flex)`
   ${css`
@@ -95,25 +70,12 @@ const StyledFlex = styled(Flex)`
   `}
 `
 
-const NewsFeed = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 1rem;
-  overflow: hidden;
-  position: relative;
-  transition: transform 0.5s ease;
-
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
-  }
-`
-
 const NewsItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 1rem;
+  padding: 0.5rem;
   background-color: #fff;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
@@ -148,42 +110,9 @@ const NewsTitle = styled.h3`
   text-align: left;
 `
 
-const SlideButton = styled.button`
-  position: absolute;
-  top: 40%;
-  display: flex;
-  align-items: center;
-  transform: translateY(-50%);
-  background-color: transparent;
-  border: 4px solid #c58fff;
-  border-radius: 50%;
-  color: #c58fff;
-  font-size: 2rem;
-  padding: 0.3rem;
-  cursor: pointer;
-  z-index: 1;
-
-  &:last-child {
-    right: -1.5rem;
-  }
-`
-
 const News = () => {
   const { t } = useTranslation()
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const handleSlide = (direction) => {
-    const numVisibleNews = window.innerWidth > 768 ? 3 : 1
-    let nextIndex
-
-    if (direction === 'prev') {
-      nextIndex = currentIndex === 0 ? newsFeedData.length - numVisibleNews : currentIndex - 1
-    } else {
-      nextIndex = (currentIndex + 1) % (newsFeedData.length - numVisibleNews + 1)
-    }
-
-    setCurrentIndex(nextIndex)
-  }
+  const [activeItemIndex, setActiveItemIndex] = useState(0)
 
   const handleNewsClick = (link) => {
     window.open(link, '_blank') // 링크를 새 탭으로 열기
@@ -202,27 +131,34 @@ const News = () => {
           {t('Featured News')}
         </Heading>
       </StyledFlex>
-      <MobileFlex>
-        <NewsFeed>
-          {newsFeedData.slice(currentIndex, currentIndex + (window.innerWidth > 768 ? 3 : 1)).map((newsItem) => (
-            <NewsItem onClick={() => handleNewsClick(newsItem.link)} key={newsItem.id}>
-              <NewsImage src={newsItem.image} alt={newsItem.title} />
-              <Flex justifyContent="space-around" width="100%">
-                <NewsAuthor>{newsItem.author}</NewsAuthor>
-                <NewsDate>{formatDate(newsItem.date)}</NewsDate>
-              </Flex>
-              <NewsTitle>{newsItem.title}</NewsTitle>
-            </NewsItem>
-          ))}
-        </NewsFeed>
-
-        <SlideButton onClick={() => handleSlide('prev')} style={{ left: '-1.5rem' }}>
-          <ArrowBackIcon />
-        </SlideButton>
-        <SlideButton onClick={() => handleSlide('next')}>
-          <ArrowForwardIcon />
-        </SlideButton>
-      </MobileFlex>
+      <ItemsCarousel
+        infiniteLoop={true}
+        gutter={4}
+        activePosition={'center'}
+        chevronWidth={30}
+        disableSwipe={false}
+        alwaysShowChevrons={true}
+        numberOfCards={window.innerWidth > 768 ? 3 : 1}
+        slidesToScroll={1}
+        outsideChevron={true}
+        showSlither={false}
+        firstAndLastGutter={false}
+        activeItemIndex={activeItemIndex}
+        requestToChangeActive={(index) => setActiveItemIndex(index)}
+        rightChevron={<ArrowForwardIcon style={{ color: 'purple' }} />}
+        leftChevron={<ArrowBackIcon style={{ color: 'purple' }} />}
+      >
+        {newsFeedData.map((newsItem) => (
+          <NewsItem onClick={() => handleNewsClick(newsItem.link)} key={newsItem.id}>
+            <NewsImage src={newsItem.image} alt={newsItem.title} />
+            <Flex justifyContent="space-around" width="100%">
+              <NewsAuthor>{newsItem.author}</NewsAuthor>
+              <NewsDate>{formatDate(newsItem.date)}</NewsDate>
+            </Flex>
+            <NewsTitle>{newsItem.title}</NewsTitle>
+          </NewsItem>
+        ))}
+      </ItemsCarousel>
     </>
   )
 }
