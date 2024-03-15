4611311271b5e9191856f6f985e4e7e93cf589f7
meritz3695 <meritz3695@gmail.com>
2024-03-15 21:21:51 +0900

    update: social links on Join component

--- src/views/Home/components/Join.tsx
+++ src/views/Home/components/Join.tsx
@@ -194,22 +194,22 @@ const Join = () => {
           </Flex>
 
           <SocialLogoGroups>
-            <a href="/" target="_blank">
+            <a href="https://www.tiktok.com/@focinofficial" target="_blank">
               <img src="/images/tiktok-icon.svg" alt="tiktok-icon" width={24} />
             </a>
-            <a href="/" target="_blank">
+            <a href="https://twitter.com/focincofficial?s=21" target="_blank">
               <img src="/images/twitter-icon.svg" alt="twitter" width={24} />
             </a>
             <a href="/" target="_blank">
               <img src="/images/instagram-icon.svg" alt="instagram" width={24} />
             </a>
-            <a href="/" target="_blank">
+            <a href="https://www.facebook.com/profile.php?id=61556776132353" target="_blank">
               <img src="/images/facebook-icon.svg" alt="facebook" width={24} />
             </a>
             <a href="/" target="_blank">
               <img src="/images/email-icon.svg" alt="email" width={24} />
             </a>
-            <a href="/" target="_blank">
+            <a href="https://discord.gg/wxk5FSWc" target="_blank">
               <img src="/images/discord-icon.svg" alt="discord" width={24} />
             </a>
           </SocialLogoGroups>
