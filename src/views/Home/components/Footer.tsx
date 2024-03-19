fadca5afedfd567c3e83ebe8d26fc9fa9d507f46
meritz3695 <meritz3695@gmail.com>
2024-03-19 17:03:43 +0900

    fix: noferrer

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -168,22 +168,22 @@ const Footer = () => {
         <GridDiv>
           <img src="/images/logo_foc.svg" alt="logo" width={150} style={{ margin: '1rem 0' }} />
           <SocialLogoGroups>
-            <a href="https://web.focad.ph/" target="_blank">
+            <a href="https://web.focad.ph/" target="_blank" rel="noreferrer">
               <HomeIcon style={{ color: '#fff', width: '26px', height: '26px' }} />
             </a>
-            <a href="https://www.tiktok.com/@focinofficial" target="_blank">
+            <a href="https://www.tiktok.com/@focinofficial" target="_blank" rel="noreferrer">
               <img src="/images/tiktok-icon-white.svg" alt="tiktok-icon" width={24} />
             </a>
-            <a href="https://twitter.com/focincofficial?s=21" target="_blank">
+            <a href="https://twitter.com/focincofficial?s=21" target="_blank" rel="noreferrer">
               <img src="/images/x-icon-white.svg" alt="twitter" width={30} />
             </a>
             {/* <a href="/" target="_blank">
               <img src="/images/instagram-icon.svg" alt="instagram" width={24} />
             </a> */}
-            <a href="https://www.facebook.com/profile.php?id=61556776132353" target="_blank">
+            <a href="https://www.facebook.com/profile.php?id=61556776132353" target="_blank" rel="noreferrer">
               <img src="/images/facebook-icon-white.svg" alt="facebook" width={24} />
             </a>
-            <a href="https://discord.gg/wxk5FSWc" target="_blank">
+            <a href="https://discord.gg/wxk5FSWc" target="_blank" rel="noreferrer">
               <img src="/images/discord-icon-white.svg" alt="discord" width={24} />
             </a>
           </SocialLogoGroups>
